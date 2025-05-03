73549df151202415c2b84f31368583f163205cad
Filipe Brandão <filipebrandao350@gmail.com>
2025-05-03 20:29:02 -0300

    Mais uma atualização do dia. O tabbar agora está funcionando perfeitamente, consigo navegar de uma pagina a outra. Deu para entender bem o conceito.

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -9,6 +9,12 @@ import { Settings } from '../screens/Tab/Settings';
 import { Notificacao } from '@src/screens/Tab/Notificacao';
 import { Relatorio } from '@src/screens/Tab/Relatorio';
 
+// Importando icones
+import { CreditCard,
+         Gear,
+         Bell,
+         ChartBar } from "phosphor-react-native";
+
 export const TabRoutes = () => {
 
   const { Navigator, Screen } = createBottomTabNavigator();
@@ -27,6 +33,7 @@ export const TabRoutes = () => {
         },
         tabBarLabelStyle: {
           fontSize: 11,
+          color: theme.COLORS.GRAY3,
           // shadowOpacity: 0.2, 
         }
       }}
@@ -34,21 +41,129 @@ export const TabRoutes = () => {
       <Screen
         name="Carteira"
         component={Carteira}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ focused }) => (
+            <View>
+              {focused ? (
+                <CreditCard
+                  size={40}
+                  color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY4}
+                  weight="fill"
+                />
+              ) : (
+                <CreditCard
+                  size={40}
+                  color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY3}
+                  weight="light"
+                />
+              )}
+
+
+              {/* <CreditCard
+                size={40}
+                color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY4}
+                weight="thin"
+              /> */}
+            </View>
+          )
+        }}
       />
 
       <Screen
         name="Notificacao"
         component={Notificacao}
+        options={{
+          tabBarLabel: 'Notificação',
+          tabBarIcon: ({ focused }) => (
+            <View>
+              {focused ? (
+                <Bell
+                  size={40}
+                  color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY4}
+                  weight="fill"
+                />
+              ) : (
+                <Bell
+                  size={40}
+                  color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY3}
+                  weight="light"
+                />
+              )}
+
+
+              {/* <CreditCard
+                size={40}
+                color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY4}
+                weight="thin"
+              /> */}
+            </View>
+          )
+        }}
       />
 
       <Screen
         name="Relatorio"
         component={Relatorio}
+        options={{
+          tabBarLabel: 'Relatório',
+          tabBarIcon: ({ focused }) => (
+            <View>
+              {focused ? (
+                <ChartBar
+                  size={40}
+                  color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY4}
+                  weight="fill"
+                />
+              ) : (
+                <ChartBar
+                  size={40}
+                  color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY3}
+                  weight="light"
+                />
+              )}
+
+
+              {/* <CreditCard
+                size={40}
+                color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY4}
+                weight="thin"
+              /> */}
+            </View>
+          )
+        }}
       />
 
       <Screen
         name="settings"
         component={Settings}
+        options={{
+          tabBarLabel: 'Configurações',
+          tabBarIcon: ({ focused }) => (
+            <View>
+              {focused ? (
+                <Gear
+                  size={40}
+                  color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY4}
+                  weight="fill"
+                />
+              ) : (
+                <Gear
+                  size={40}
+                  color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY3}
+                  weight="light"
+                />
+              )}
+
+
+              {/* <CreditCard
+                size={40}
+                color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY4}
+                weight="thin"
+              /> */}
+            </View>
+          )
+        }}
       />
 
     </Navigator>
--- src/screens/Tab/settings/styles.ts
+++ src/screens/Tab/settings/styles.ts
@@ -3,7 +3,8 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1 ;
-    background-color: ${theme.COLORS.PINK};
+    justify-content: center;
+    align-items: center;
 `;
 
 export const Title = styled.Text`
