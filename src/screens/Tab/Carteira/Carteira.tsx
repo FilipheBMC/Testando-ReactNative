5ae43c30a6b79a2a57b6159314c4c50eb2147c6a
Filipe Brandão <filipebrandao350@gmail.com>
2025-05-05 15:59:35 -0300

    Mais uma atualização, na próxima vou conseguir terminar esse menu inicial! Agora é ir para a facul!

--- src/screens/Tab/Carteira/Carteira.tsx
+++ src/screens/Tab/Carteira/Carteira.tsx
@@ -9,11 +9,43 @@ import {
   TitleValorConta,
   TitleCartao,
   TitleNomeCartao,
+  Body,
+  IconTransfer,
+  IconPayment,
+  IconTapOut,
+  IconPayOut,
+  TitleTransfer,
+  TitlePayment,
+  TitleTapOut,
+  TitlePayOut,
+  Footer,
+
+  ContentFlat,
+  ContentFlatHeader,
+  Title,
+  ButtonVerTodos,
+  ButtonTitleVerTodos,
+  IconTransaction,
+  DetailsTransaction,
+  NameTransaction,
+  SubTitleTransaction,
+  PriceTransaction,
 
 } from "./styles";
 
+// Importando as imagens
+
+import Transfer from "../../../assets/convert.png";
+import Payment from "../../../assets/export.png";
+import Payout from "../../../assets/money-send.png";
+import TopOut from "../../../assets/add-circle.png";
+
+// Finalizado as importações das imagens par ao body
+
+import { transaction } from '../../../Util/transaction';
 import { Header } from "../../../components/Header/Header";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { FlatList } from 'react-native-gesture-handler';
 
 export const Carteira = () => {
   return (
@@ -33,7 +65,62 @@ export const Carteira = () => {
           </ViewBalanceRight>
 
         </Content>
+
+
+        <Body>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <IconTransfer source={Transfer} />
+            <TitleTransfer>Transferência</TitleTransfer>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <IconPayment source={Payment} />
+            <TitlePayment>Pagamentos</TitlePayment>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <IconTapOut source={TopOut} />
+            <TitleTapOut>Mais Opções</TitleTapOut>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <IconPayOut source={Payout} />
+            <TitlePayOut>Pagamento</TitlePayOut>
+          </TouchableOpacity>
+        </Body>
+
       </ViewContainer>
+
+      <Footer>
+
+        <FlatList
+          data={transaction}
+          renderItem={({ item }) => (
+            <ContentFlat>
+
+              <IconTransaction />
+              <DetailsTransaction>
+
+                <NameTransaction>Netflix</NameTransaction>
+                <SubTitleTransaction>Valor da Netflix</SubTitleTransaction>
+
+              </DetailsTransaction>
+
+              <PriceTransaction>R$:20</PriceTransaction>
+
+            </ContentFlat>
+          )}
+          ListHeaderComponent={
+            <ContentFlatHeader>
+
+              <Title>Minhas transaçõe</Title>
+
+              <ButtonVerTodos>
+                <ButtonTitleVerTodos>Ver todas</ButtonTitleVerTodos>
+              </ButtonVerTodos>
+
+            </ContentFlatHeader>
+          }
+        />
+
+      </Footer>
     </Container>
   )
 }
--- src/screens/Tab/Carteira/styles.ts
+++ src/screens/Tab/Carteira/styles.ts
@@ -1,12 +1,9 @@
 import theme from "@src/styles/theme";
+import { Pressable } from "react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
-`;
-
-export const Title = styled.Text`
-    
 `;
 
 export const Content = styled.View`
@@ -38,9 +35,128 @@ export const TitleValue = styled.Text`
 export const TitleValorConta = styled.Text`
     font-size: 19px;
     color: ${theme.COLORS.WHITE};
+    font-family: ${theme.FONTS.POPPINSBOLD};
+`;
+
+export const TitleCartao = styled.Text`
+    font-size: 16px;
+    color: ${theme.COLORS.WHITE};
+    font-family: ${theme.FONTS.POPPINSREGULAR};
+`;
+
+export const TitleNomeCartao = styled.Text`
+    font-size: 16px;
+    color: ${theme.COLORS.WHITE};
+    font-family: ${theme.FONTS.POPPINSBOLD};
+`;
+
+export const Body = styled.View`
+    width: 100%;
+    padding: 10px;
+    margin-top: 20px;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: row;
+
+`;
+
+// Icones
+
+export const IconTransfer = styled.Image`
+    width: 70px;
+    height: 70px;
+`;
+
+export const IconPayment = styled.Image`
+    width: 70px;
+    height: 70px;
+`;
+
+export const IconTapOut = styled.Image`
+    width: 70px;
+    height: 70px;
+`;
+
+export const IconPayOut = styled.Image`
+    width: 70px;
+    height: 70px;
+`;
+
+export const TitleTransfer = styled.Text`
+    font-size: 11px;
+    color: ${theme.COLORS.PURPLE};
     font-family: ${theme.FONTS.POPPINSMEDIUM};
 `;
 
-export const TitleCartao = styled.Text``;
+export const TitlePayment = styled.Text`
+    font-size: 11px;
+    color: ${theme.COLORS.PURPLE};
+    font-family: ${theme.FONTS.POPPINSMEDIUM};
+`;
 
-export const TitleNomeCartao = styled.Text``;
+export const TitleTapOut = styled.Text`
+    font-size: 11px;
+    color: ${theme.COLORS.PURPLE};
+    font-family: ${theme.FONTS.POPPINSMEDIUM};
+`;
+
+export const TitlePayOut = styled.Text`
+    font-size: 11px;
+    color: ${theme.COLORS.PURPLE};
+    font-family: ${theme.FONTS.POPPINSMEDIUM};
+`;
+
+export const Footer = styled.View`
+    flex: 1;
+    width: 100%;
+`;
+
+export const ContentFlat = styled.View`
+    display: 1;
+    padding: 20px;
+`;
+
+export const ContentFlatHeader = styled.View`
+    flex-direction: row;
+    padding: 10px;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const ButtonVerTodos = styled.TouchableOpacity`
+    border-width: 2px;
+    border-radius: 30px;
+    padding: 5px;
+    border-color: ${theme.COLORS.PURPLE2};
+`;
+
+export const ButtonTitleVerTodos = styled.Text`
+
+`;
+
+export const IconTransaction = styled.Image`
+
+`;
+
+export const DetailsTransaction = styled.View`
+
+`;
+
+export const NameTransaction = styled.Text`
+
+`;
+
+export const SubTitleTransaction = styled.Text`
+
+`;
+
+export const PriceTransaction = styled.Text`
+
+`
+;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: ${theme.COLORS.GRAY1};
+  font-family: ${theme.FONTS.POPPINSBOLD};
+`;
